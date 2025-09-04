f6c6d286f40cb621303b39f776490aaf5a5b4354
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-09-04 19:42:35 +0900

    [#1] Color+Extension 파일 추가

--- Clokey/Application/ClokeyApp.swift
+++ Clokey/Application/ClokeyApp.swift
@@ -4,26 +4,19 @@ import SwiftUI
 struct ClokeyApp: App {
     var body: some Scene {
         WindowGroup {
-            Text(
-                "Title 1 (Semibold 20)"
-            )
-            .font(
-                .clokey_title1
-            )
+            Text("Title 1 (Semibold 20)")
+            .font(.clokey_title1)
+            .foregroundColor(.CloKey.grayscale1)
             
-            Text(
-                "Body 1 (Medium 16)"
-            )
-            .font(
-                .clokey_body1_medium
-            )
+            Text("Title 1 (Semibold 20)")
+            .font(.system(size: 20, weight: .semibold))
+            .foregroundColor(.CloKey.grayscale1)
             
-            Text(
-                "Body 2 (Regular 14)"
-            )
-            .font(
-                .clokey_body2_regular
-            )
+            Text("Body 1 (Medium 16)")
+            .font(.clokey_body1_medium)
+            
+            Text("Body 2 (Regular 14)")
+            .font(.clokey_body2_regular)
         }
     }
 }
---
+++ Clokey/Core/Extensions/Color+Extension.swift
@@ -0,0 +1,35 @@
+//
+//  Color+Extension.swift
+//  Clokey
+//
+//  Created by 황상환 on 9/4/25.
+//
+
+import SwiftUI
+
+extension Color {
+    enum CloKey {
+        // Grayscale
+        static var grayscale1: Color { Color("Grayscale1") }
+        static var grayscale2: Color { Color("Grayscale2") }
+        static var grayscale3: Color { Color("Grayscale3") }
+        static var grayscale4: Color { Color("Grayscale4") }
+        static var grayscale5: Color { Color("Grayscale5") }
+        static var grayscale6: Color { Color("Grayscale6") }
+        static var grayscale7: Color { Color("Grayscale7") }
+        
+        // Main
+        static var main0: Color { Color("main0") }
+        static var main1: Color { Color("main1") }
+        static var main2: Color { Color("main2") }
+        static var main3: Color { Color("main3") }
+        static var main4: Color { Color("main4") }
+        static var main5: Color { Color("main5") }
+        static var main6: Color { Color("main6") }
+        
+        // Point
+        static var point1: Color { Color("point1") }
+        static var point2: Color { Color("point2") }
+        static var point3: Color { Color("point3") }
+    }
+}
