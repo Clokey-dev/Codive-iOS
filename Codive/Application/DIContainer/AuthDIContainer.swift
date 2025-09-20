484c116a26d1139aef67481efc91784fc3a55d61
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-09-20 17:42:24 +0900

    [#2] DI 파일 추가 및 Router 기본 세팅파일 추가

--- Codive/Application/DIContainer/AuthDIContainer.swift
+++ Codive/Application/DIContainer/AuthDIContainer.swift
@@ -1,7 +1,8 @@
 //
-//  AppDIContainer.swift
+//  AuthDIContainer.swift
 //  Codive
 //
 //  Created by 황상환 on 9/20/25.
 //
 
+import Foundation
---
+++ Codive/Presentation/Router/Destination.swift
@@ -0,0 +1,27 @@
+//
+//  Destination.swift
+//  Codive
+//
+//  Created by 황상환 on 9/20/25.
+//
+
+import Foundation
+
+enum Destination: Hashable {
+    
+    case login
+    case signup
+    case main
+    
+    // 각 목적지의 고유 식별자
+    var id: String {
+        switch self {
+        case .login:
+            return "login"
+        case .signup:
+            return "signup"
+        case .main:
+            return "main"
+        }
+    }
+}
