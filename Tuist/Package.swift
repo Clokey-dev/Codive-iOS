f3852ba3c1f9673cc579be51ed3674ed93673f69
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-10-02 14:53:39 +0900

    [#4] Tuist 폴더 누락 버그 수정

---
+++ Tuist/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+#if TUIST
+    import struct ProjectDescription.PackageSettings
+
+    let packageSettings = PackageSettings(
+        // Customize the product types for specific package product
+        // Default is .staticFramework
+        // productTypes: ["Alamofire": .framework,]
+        productTypes: [:]
+    )
+#endif
+
+let package = Package(
+    name: "Codive",
+    dependencies: [
+        // 카카오 SDK
+        .package(url: "https://github.com/kakao/kakao-ios-sdk", from: "2.22.5"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0"),
+    ]
+)
