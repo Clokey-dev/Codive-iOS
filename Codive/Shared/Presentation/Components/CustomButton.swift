549ba187572a00bb4f0c4dc6ffee0f01cb9abe72
Funital <gksrmawns0925@naver.com>
2025-10-04 23:20:58 +0900

    [#7] CustomButton 구현
    #7

---
+++ Codive/Shared/Presentation/Components/CustomButton.swift
@@ -0,0 +1,94 @@
+//
+//  CustomButton.swift
+//  Codive
+//
+//  Created by 한금준 on 10/4/25.
+//
+
+import SwiftUI
+
+enum ButtonType {
+    case fixed
+    case dynamic
+    case half
+}
+
+struct CustomButton: View {
+    let text: String
+    let widthType: ButtonType
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .font(Font.codive_title2)
+                .padding()
+                .foregroundColor(.white)
+        }
+        .modifier(WidthModifier(type: widthType))
+        .frame(height: 48)
+        .background(Color.Codive.main0)
+        .cornerRadius(10)
+    }
+}
+
+struct WidthModifier: ViewModifier {
+    let type: ButtonType
+    
+    func body(content: Content) -> some View {
+        switch type {
+        case .fixed:
+            content
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 20)
+        case .dynamic:
+            content
+                .padding(.horizontal, 16)
+        case .half:
+            content
+                .frame(width: UIScreen.main.bounds.width / 2 - 20)
+        }
+    }
+}
+
+#Preview {
+    VStack(spacing: 20) {
+        /// 1. 6.  화면 전체 너비 버튼
+        CustomButton(text: "이 코디로 결정하기", widthType: .fixed) {
+            print("Fixed Button tapped!")
+        }
+        
+        /// 5. 고정크기 버튼
+        CustomButton(text: "피드 작성하러 가기", widthType: .dynamic) {
+            print("Dynamic Button tapped!")
+        }
+    }
+    .padding()
+    
+    /// 3. 4. 화면 전체 너비 1/2
+    HStack(spacing: 9) {
+        CustomButton(text: "이전으로", widthType: .half) {
+            print("이전으로 tapped")
+        }
+        CustomButton(text: "등록하기", widthType: .half) {
+            print("등록하기 tapped")
+        }
+    }
+    .padding()
+    
+    CustomButton(text: "Half 버튼", widthType: .half) {
+        print("Half Button tapped!")
+    }
+    
+    /// 2. 서로 다른 너비
+    HStack(spacing: 16) {
+        CustomButton(text: "코디보드", widthType: .dynamic) {
+            print("이전으로 tapped")
+        }
+        
+        CustomButton(text: "이 코디로 결정하기", widthType: .dynamic) {
+            print("등록하기 tapped")
+        }
+    }
+    .padding()
+}
