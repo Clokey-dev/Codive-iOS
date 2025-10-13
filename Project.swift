5e1cd52afb948d7c937a66519a34304a385d3ac3
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2025-10-13 16:48:28 +0900

    [#13] 갤러리, 카메라 권한 추가

--- Project.swift
+++ Project.swift
@@ -79,6 +79,11 @@ let project = Project(
                     "CFBundleDisplayName": "$(PRODUCT_NAME)",
                     "BASE_URL": "$(BASE_URL)",
                     
+                    // 갤러리 & 카메라 권한 추가
+                    "NSPhotoLibraryUsageDescription": "기록을 추가하기 위해 사진 접근 권한이 필요합니다.",
+                    "NSPhotoLibraryAddUsageDescription": "사진을 저장하기 위해 권한이 필요합니다.",
+                    "NSCameraUsageDescription": "사진을 촬영하기 위해 카메라 권한이 필요합니다.",
+                    
                     // 카카오 SDK 설정
                     "KAKAO_APP_KEY": "$(KAKAO_APP_KEY)",
                     "CFBundleURLTypes": [
