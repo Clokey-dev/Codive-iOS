16c6aca8d98051cdf2cc5302f79b3c6d861a0823
Funital <gksrmawns0925@naver.com>
2025-11-11 01:25:30 +0900

    [#12] 디버깅 코드 제거 추가 작업
    #12

--- Codive/Features/Home/Presentation/ViewModel/HomeViewModel.swift
+++ Codive/Features/Home/Presentation/ViewModel/HomeViewModel.swift
@@ -17,7 +17,7 @@ final class HomeViewModel: ObservableObject {
     @Published var selectedIndex: Int? = 0
     @Published var showClothSelector: Bool = false
     @Published var titleFrame: CGRect = .zero
-
+    
     // 날씨 데이터 상태 추가
     @Published var weatherData: WeatherData?
     @Published var weatherErrorMessage: String?
@@ -25,10 +25,10 @@ final class HomeViewModel: ObservableObject {
     @Published var todayString: String = ""
     @Published var selectedItemID: Int?
     @Published var codiItems: [CodiItemEntity] = []
-
+    
     private let navigationRouter: NavigationRouter
     private let useCase: HomeUseCase
-
+    
     init(navigationRouter: NavigationRouter, useCase: HomeUseCase) {
         self.navigationRouter = navigationRouter
         self.useCase = useCase
@@ -36,7 +36,7 @@ final class HomeViewModel: ObservableObject {
         loadDummyCodi()
         loadToday()
     }
-
+    
     // WeatherKit 데이터 불러오기
     func loadWeather(for location: CLLocation) async {
         do {
@@ -47,34 +47,28 @@ final class HomeViewModel: ObservableObject {
             weatherErrorMessage = "날씨 정보를 가져오는 데 실패했습니다. 잠시 후 다시 시도해주세요."
         }
     }
-
+    
     func toggleClothSelector() {
         withAnimation(.spring()) {
             showClothSelector.toggle()
         }
     }
-
+    
     func selectCloth(at index: Int) {
         selectedIndex = index
     }
-
-    func handleSearchTap() {
-        print("검색 버튼 클릭")
-    }
-
-    func handleNotificationTap() {
-        print("알림 버튼 클릭")
-    }
-
+    
+    func handleSearchTap() {}
+    
+    func handleNotificationTap() {}
+    
     func handleCodiBoardTap() {
         navigationRouter.navigate(to: .codiBoard)
-        print("코디보드 tapped")
     }
-
+    
     func handleConfirmCodiTap() {
-        print("이 코디 결정 tapped")
     }
-
+    
     func handleEditCategory() {
         navigationRouter.navigate(to: .editCategory)
     }
@@ -92,19 +86,11 @@ final class HomeViewModel: ObservableObject {
         self.todayString = entity.formattedDate
     }
     
-    func rememberCodi() {
-        print("오늘 이 코디를 기억")
-    }
+    func rememberCodi() {}
     
-    func selectEditCodi() {
-        print("코디 수정 tapped")
-    }
+    func selectEditCodi() {}
     
-    func addLookbook() {
-        print("룩북에 추가 tapped")
-    }
+    func addLookbook() {}
     
-    func sharedCodi() {
-        print("코디 공유 tapped")
-    }
+    func sharedCodi() {}
 }
