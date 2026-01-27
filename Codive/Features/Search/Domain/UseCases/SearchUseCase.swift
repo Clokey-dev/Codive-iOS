899b9d2cc7c4261497cb01273cf88ccf9a630cc9
Funital <gksrmawns0925@naver.com>
2026-01-28 01:08:11 +0900

    [#51] 검색 관련 api에 대한 Usecase 추가
    #51

--- Codive/Features/Search/Domain/UseCases/SearchUseCase.swift
+++ Codive/Features/Search/Domain/UseCases/SearchUseCase.swift
@@ -5,6 +5,8 @@
 //  Created by 한금준 on 11/14/25.
 //
 
+import CodiveAPI
+
 final class SearchUseCase {
     // MARK: - Properties
     private let repository: SearchRepository
@@ -15,7 +17,53 @@ final class SearchUseCase {
     }
     
     // MARK: - Fetch Methods
-    
+
+    /// 검색 탭 기록 추천
+    func fetchSearchRecommendation() async throws -> [SearchRecommendationEntity] {
+        return try await repository.fetchSearchRecommendation()
+    }
+
+    /// 유저 검색
+    func fetchSearchMembers(
+        keyword: String,
+        page: Int64,
+        size: Int32
+    ) async throws -> (content: [SearchMembersEntity], isLast: Bool) {
+        return try await repository.fetchSearchMembers(keyword: keyword, page: page, size: size)
+    }
+
+    /// 기록 검색
+    func fetchSearchHistories(
+        keyword: String,
+        page: Int64,
+        size: Int32,
+        sort: Operations.Search_searchHistoryByHashtagsAndCategories.Input.Query.sortPayload?
+    ) async throws -> (content: [SearchHistoriesEntity], isLast: Bool) {
+        return try await repository.fetchSearchHistories(keyword: keyword, page: page, size: size, sort: sort)
+    }
+
+    /// 전체 유저 검색 엔진 삭제(개발용)
+    func fetchSearchMembersUnsyncAll() async throws {
+        try await repository.fetchSearchMembersUnsyncAll()
+    }
+
+    /// 전체 유저 검색 엔진 동기화(개발용)
+    func fetchSearchMembersSyncAll() async throws {
+        try await repository.fetchSearchMembersSyncAll()
+    }
+
+    /// 전체 기록 검색 엔진 삭제(개발용)
+    func fetchSearchHistoryUnsyncAll() async throws {
+        try await repository.fetchSearchHistoryUnsyncAll()
+    }
+
+    /// 전체 기록 검색 엔진 동기화(개발용)
+    func fetchSearchHistorySyncAll() async throws {
+        try await repository.fetchSearchHistorySyncAll()
+    }
+}
+
+extension SearchUseCase {
     func fetchUserName() -> SearchEntity {
         return repository.fetchUserName()
     }
