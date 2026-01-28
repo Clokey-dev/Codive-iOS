c501381743916442f987242d8a0d19d5a0366026
Funital <gksrmawns0925@naver.com>
2026-01-28 22:53:54 +0900

    [#51] 홈 관련 api에 대한 UseCase 추가
    #51

--- Codive/Features/Home/Domain/UseCases/TodayCodiUseCase.swift
+++ Codive/Features/Home/Domain/UseCases/TodayCodiUseCase.swift
@@ -6,13 +6,27 @@
 //
 
 final class TodayCodiUseCase {
-
+    
     private let repository: HomeRepository
-
+    
     init(repository: HomeRepository) {
         self.repository = repository
     }
+    
+    /// 오늘의 코디 생성
+    func createTodayCoordinate(
+        request: CreateTodayCoordinateRequestDTO
+    ) async throws -> TodayCoordinateEntity {
+        try await repository.createTodayCoordinate(request: request)
+    }
+    
+    /// 오늘의 코디 옷 정보 조회
+    func fetchTodayCoordinateClothes() async throws -> [TodayCoordinateClothEntity] {
+        try await repository.fetchTodayCoordinateClothes()
+    }
+}
 
+extension TodayCodiUseCase {
     func loadTodaysCodi() -> [CodiItemEntity] {
         return repository.fetchCodiItems()
     }
