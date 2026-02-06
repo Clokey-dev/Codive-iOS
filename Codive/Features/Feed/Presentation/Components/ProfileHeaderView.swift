f0b7e8b4a7b9ca6b932a32467a852ca9f5f71f51
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2026-02-07 00:28:38 +0900

    [#61] FeedDetailView 프로필 이미지 연결

--- Codive/Features/Feed/Presentation/Components/ProfileHeaderView.swift
+++ Codive/Features/Feed/Presentation/Components/ProfileHeaderView.swift
@@ -15,15 +15,25 @@ struct ProfileHeaderView: View {
     
     var body: some View {
         HStack(spacing: 8) {
-            // TODO: API 연동 시 profileImageUrl을 사용하여 비동기 이미지 로딩 (KingFisher 또는 AsyncImage)
-            Image("Profile")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 32, height: 32)
-                .clipShape(Circle())
-                .onTapGesture {
-                    onProfileTap()
+            AsyncImage(url: URL(string: profileImageUrl)) { phase in
+                switch phase {
+                case .success(let image):
+                    image
+                        .resizable()
+                        .scaledToFill()
+                case .failure, .empty:
+                    Image("Profile")
+                        .resizable()
+                        .scaledToFill()
+                @unknown default:
+                    EmptyView()
                 }
+            }
+            .frame(width: 32, height: 32)
+            .clipShape(Circle())
+            .onTapGesture {
+                onProfileTap()
+            }
 
             Text(nickname)
                 .font(.codive_body2_medium)
