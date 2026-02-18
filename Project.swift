bdf42a8b49b652e73c9f50522507d3f51fbfbc2d
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2026-02-18 15:56:57 +0900

    [#64] 위치 권한 문구 변경 및 패키지 업데이트

--- Project.swift
+++ Project.swift
@@ -88,7 +88,7 @@ let project = Project(
                     "NSCameraUsageDescription": "사진을 촬영하기 위해 카메라 권한이 필요합니다.",
                     
                     // 위치 권한 추가
-                    "NSLocationWhenInUseUsageDescription": "위치한 지역의 날씨 정보를 제공하기 위해 현재 위치를 확인합니다.",
+                    "NSLocationWhenInUseUsageDescription": "위치한 지역의 날씨 정보를 통해 옷을 추천해드리기 위해 현재 위치를 확인합니다.",
                     
                     // 카카오 SDK 설정
                     "KAKAO_APP_KEY": "$(KAKAO_APP_KEY)",
