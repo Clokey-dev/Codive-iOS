4e958c9829d511c770fb75b17e51fd4be2cfc357
Hwang Sang Hwan <137605270+Hrepay@users.noreply.github.com>
2026-03-24 22:37:49 +0900

    [#75] 코디 업로드 버튼 크기를 텍스트에 맞게 수정

--- Codive/Features/LookBook/Presentation/View/AddCodiView.swift
+++ Codive/Features/LookBook/Presentation/View/AddCodiView.swift
@@ -156,6 +156,7 @@ private extension AddCodiView {
                 ) {
                     viewModel.handleCodiUploadTap()
                 }
+                .fixedSize()
             }
     }
 }
